bf33c979bab62385f7e705c336625e696c8d1e31
parthbonami <parth@bonamisoftware.com>
2022-10-01 13:19:39 +0530

    Loop implementations

---
+++ loops.py
@@ -0,0 +1,33 @@
+# while implementation
+print("\n while loop")
+i=0
+while(i<10) :
+    print(i)
+    i +=1
+
+print("\n\n for loop")
+#for implementation
+for i in range(5) :
+    print(i)
+
+print("\n\n")
+n = 10
+for i in range(0,n):
+    print(n-i)
+
+print("\n\n")
+set = {1,2,3,4,5,6,1,2,3}
+for i in set:
+    print(i)
+
+
+print("\n\nNested Loops")
+for i in range(1,5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+#for loop different iterator
+for i in range(1,10,2):
+    print(i,end=", ")
+
